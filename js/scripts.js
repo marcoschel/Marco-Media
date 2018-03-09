ac22ad2f19390afb30a046c6841852b71e583de8
Marco <MarcoSchel@MacBook-Pro-van-Marco.local>
2018-03-09 22:26:43 +0100

    opmaak alless

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+/* influenced by IVAN LAZAREVIC'S jQuery snow http://workshop.rs/projects/jqsnow/ */
+
+function $snow() {
+  var snow = $('<div id="snow"/>').css({
+      'position': 'absolute',
+      'top': '-50px'
+    }).html('	&#x2022;'),
+    animationHeight = $('.snow-container').height(),
+    animationWidth = $(document).width(),
+    snowCount = 600;
+
+  setInterval(function() {
+
+    var startPositionLeft = Math.random() * animationWidth,
+      startPositionTop = 0,
+      startOpacity = 1,
+      duration = Math.random() * animationHeight + 7000,
+      snowCol = '#fff',
+      endPositionLeft = startPositionLeft - 100 + Math.random() * 200,
+      endPositionTop = animationHeight + 100;
+
+    snow.clone().appendTo('.snow-container').css({
+      left: startPositionLeft,
+      top: startPositionTop,
+      opacity: startOpacity,
+      'color': snowCol
+
+    }).animate({
+        opacity: 0,
+        top: endPositionTop,
+        left: endPositionLeft,
+      },
+      duration,
+      'linear',
+
+      function() {
+        $(this).remove();
+      });
+  }, snowCount);
+
+}
+
+$(document).ready(function() {
+  $snow();
+});
